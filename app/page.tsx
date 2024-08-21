ef4be68957d088ff1cc32f043d3e8c3f4b442d18
mikhaelHan <hancharuk270185@mail.ru>
2024-08-22 00:03:06 +0300

    fix: fix project configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
-'use client';
-import { useState } from 'react';
 import style from './page.module.scss';
 import Button from '@mui/material/Button';
 
 const HomePageComponent = () => {
-  const [state, setState] = useState<boolean>(false);
   return (
-    <div
-      style={{ background: state ? '#ffffff' : '#808080' }}
-      className={style.wrapper}
-    >
+    <div style={{ background: '#808080' }} className={style.wrapper}>
       <h1 className={style.title}>REST/GraphiQL Client</h1>
-      <Button onClick={() => setState((prev) => !prev)} variant="contained">
-        Contained
-      </Button>
+      <Button variant="contained">Contained</Button>
     </div>
   );
 };
