e5ac27e019be53df64b660659a83c453c954cbaf
mikhaelHan <hancharuk270185@mail.ru>
2024-08-25 21:44:50 +0300

    refactor: refactor tests

--- __tests__/Login.component.test.tsx
+++ __tests__/Login.component.test.tsx
@@ -5,102 +5,42 @@ import LoginComponent from '@widgets/authorization/Login.component';
 import { Provider } from 'react-redux';
 import userEvent from 'user-event';
 
-describe('Component', () => {
-  it('renders Login component with inclide the title', () => {
+describe('Login component', () => {
+  beforeEach(() => {
     render(
       <Provider store={store}>
         <LoginComponent />
       </Provider>,
     );
+  });
 
+  it('renders with inclide the title', () => {
     const title = screen.getByRole('heading', { name: 'Login' });
     expect(title).toBeInTheDocument();
   });
-});
 
-describe('Component', () => {
-  it('renders Login component with the input field "email" must accept text', () => {
-    render(
-      <Provider store={store}>
-        <LoginComponent />
-      </Provider>,
-    );
-
-    const input = screen.getByRole('textbox', { name: 'Email' });
-    expect(input).toBeInTheDocument();
+  it('renders with the input field "email" must accept text', () => {
+    const inputEmail = screen.getByRole('textbox', { name: 'Email' });
+    expect(inputEmail).toBeInTheDocument();
 
     act(() => {
-      userEvent.type(input, 'example@example.com');
+      userEvent.type(inputEmail, 'example@example.com');
+      expect(inputEmail).toHaveValue('example@example.com');
     });
-
-    expect(input).toHaveValue('example@example.com');
   });
-});
 
-describe('Component', () => {
-  it('renders Login component with the input field "password" must accept text', () => {
-    render(
-      <Provider store={store}>
-        <LoginComponent />
-      </Provider>,
-    );
-
-    const input = screen.getByLabelText('Password');
-    expect(input).toBeInTheDocument();
+  it('renders with the input field "password" must accept text', () => {
+    const inputPassword = screen.getByLabelText('Password');
+    expect(inputPassword).toBeInTheDocument();
 
     act(() => {
-      userEvent.type(input, 'Qwerty1234#');
+      userEvent.type(inputPassword, 'Qwerty1234#');
+      expect(inputPassword).toHaveValue('Qwerty1234#');
     });
-
-    expect(input).toHaveValue('Qwerty1234#');
   });
-});
 
-describe('Component', () => {
-  it('renders Login component with the input field "confirmPassword" must accept text', () => {
-    render(
-      <Provider store={store}>
-        <LoginComponent />
-      </Provider>,
-    );
-
-    const input = screen.getByLabelText('Confirm password');
-    expect(input).toBeInTheDocument();
-
-    act(() => {
-      userEvent.type(input, 'Qwerty1234#');
-    });
-
-    expect(input).toHaveValue('Qwerty1234#');
-  });
-});
-
-describe('Component', () => {
-  it('renders LoginComponent component with the Login button', () => {
-    render(
-      <Provider store={store}>
-        <LoginComponent />
-      </Provider>,
-    );
-
+  it('renders with the Login button', () => {
     const button = screen.getByTestId('Login');
-    expect(button).toBeInTheDocument();
-
-    act(() => {
-      fireEvent.click(button);
-    });
-  });
-});
-
-describe('Component', () => {
-  it('renders LoginComponent component with the Logout button', () => {
-    render(
-      <Provider store={store}>
-        <LoginComponent />
-      </Provider>,
-    );
-
-    const button = screen.getByTestId('Logout');
     expect(button).toBeInTheDocument();
 
     act(() => {
--- __tests__/Registration.component.test.tsx
+++ __tests__/Registration.component.test.tsx
@@ -5,84 +5,41 @@ import RegistrationComponent from '@widgets/authorization/Registration.component
 import { Provider } from 'react-redux';
 import userEvent from 'user-event';
 
-describe('Component', () => {
-  it('renders Registration component with inclide the title', () => {
+describe('Registration component', () => {
+  beforeEach(() => {
     render(
       <Provider store={store}>
         <RegistrationComponent />
       </Provider>,
     );
+  });
 
+  it('renders with inclide the title', () => {
     const title = screen.getByRole('heading', { name: 'Registration' });
     expect(title).toBeInTheDocument();
   });
-});
 
-describe('Component', () => {
-  it('renders RegistrationComponent component with the input field "email" must accept text', () => {
-    render(
-      <Provider store={store}>
-        <RegistrationComponent />
-      </Provider>,
-    );
-
+  it('renders with the input field "email" must accept text', () => {
     const input = screen.getByRole('textbox', { name: 'Email' });
     expect(input).toBeInTheDocument();
 
     act(() => {
       userEvent.type(input, 'example@example.com');
+      expect(input).toHaveValue('example@example.com');
     });
-
-    expect(input).toHaveValue('example@example.com');
   });
-});
 
-describe('Component', () => {
-  it('renders RegistrationComponent component with the input field "password" must accept text', () => {
-    render(
-      <Provider store={store}>
-        <RegistrationComponent />
-      </Provider>,
-    );
-
+  it('renders with the input field "password" must accept text', () => {
     const input = screen.getByLabelText('Password');
     expect(input).toBeInTheDocument();
 
     act(() => {
       userEvent.type(input, 'Qwerty1234#');
+      expect(input).toHaveValue('Qwerty1234#');
     });
-
-    expect(input).toHaveValue('Qwerty1234#');
   });
-});
 
-describe('Component', () => {
-  it('renders RegistrationComponent component with the input field "confirmPassword" must accept text', () => {
-    render(
-      <Provider store={store}>
-        <RegistrationComponent />
-      </Provider>,
-    );
-
-    const input = screen.getByLabelText('Confirm password');
-    expect(input).toBeInTheDocument();
-
-    act(() => {
-      userEvent.type(input, 'Qwerty1234#');
-    });
-
-    expect(input).toHaveValue('Qwerty1234#');
-  });
-});
-
-describe('Component', () => {
-  it('renders RegistrationComponent component with the Registration button', () => {
-    render(
-      <Provider store={store}>
-        <RegistrationComponent />
-      </Provider>,
-    );
-
+  it('renders with the Registration button', () => {
     const button = screen.getByTestId('Registration');
     expect(button).toBeInTheDocument();
 
