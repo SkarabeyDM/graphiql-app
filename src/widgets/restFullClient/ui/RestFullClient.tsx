910b36f658e95ba246a2503c0f0eaf1602ea3e4f
mikhaelHan <hancharuk270185@mail.ru>
2024-09-04 14:25:38 +0300

    feat: add Endpoint URL section in RestFullClient

--- src/widgets/restFullClient/ui/RestFullClient.tsx
+++ src/widgets/restFullClient/ui/RestFullClient.tsx
@@ -1,16 +1,27 @@
 'use client';
 import { useState } from 'react';
-import { Box } from '@mui/material';
+import { Box, TextField } from '@mui/material';
 
 import MethodManager from './MethodManager';
 import { ICRUD } from '../model/methodManagerModel';
 
 const RestFullClient = () => {
   const [method, setMethod] = useState<ICRUD>(ICRUD.GET);
+  const [url, setUrl] = useState<string>('');
+
   return (
     <Box>
       <h2>RESTfull client</h2>
+
       <MethodManager method={method} setMethod={setMethod} />
+
+      <TextField
+        fullWidth
+        margin="normal"
+        label="Endpoint URL"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
     </Box>
   );
 };
