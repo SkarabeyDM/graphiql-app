7e44549fdec42c3178497ba77a39b11929b92c57
Jamalutdin Muslimov <mechanoid0415@gmail.com>
2024-09-09 13:30:56 +0300

    fix: fix `next-intl` middleware matcher with hardcoding

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,9 @@
 import createMiddleware from 'next-intl/middleware';
-import { routing, Locales } from '@shared/i18n';
+import { routing } from '@shared/i18n';
 
 export default createMiddleware(routing);
 
 export const config = {
   // Match only internationalized pathnames
-  matcher: [
-    '/',
-    `/(${Locales.English}|${Locales.Russian})/:path*`,
-    '/((?!api|_next|_vercel|.*/..*).*)',
-  ],
+  matcher: ['/', `/(en|ru)/:path*`, '/((?!api|_next|_vercel|.*\\..*).*)'],
 };
