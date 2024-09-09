2d95dbdf0c9a906de44e9ecf9ffd8c8fc897a91c
Jamalutdin Muslimov <mechanoid0415@gmail.com>
2024-09-09 16:08:03 +0300

    refactor: change path to `HomePage` component

--- src/shared/tests/__tests__/HomePage.test.tsx
+++ src/shared/tests/__tests__/HomePage.test.tsx
@@ -1,11 +1,11 @@
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
-import HomePageComponent from '@/app/page';
 import { renderWithProviders } from '../lib';
+import { HomePage } from '@pages/HomePage';
 
 describe('HomePage component', () => {
   it('renders with include the title', () => {
-    renderWithProviders(<HomePageComponent />);
+    renderWithProviders(<HomePage />);
 
     const title = screen.getByRole('heading', { name: 'REST/GraphiQL Client' });
     expect(title).toBeInTheDocument();
