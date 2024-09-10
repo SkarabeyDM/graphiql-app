61d29fc13c9945a608e5bc1656f31c9c7c108b27
Jamalutdin Muslimov <mechanoid0415@gmail.com>
2024-09-10 11:32:35 +0300

    refactor: remove unnecessary turn off react strict mode

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/shared/i18n/request.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false };
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
