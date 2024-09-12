fa34d29f969d89cfabdeb9c139954419c1db292b
Jamalutdin Muslimov <mechanoid0415@gmail.com>
2024-09-12 09:33:44 +0300

    test: fix test errors

--- src/shared/tests/__tests__/HomePage.test.tsx
+++ src/shared/tests/__tests__/HomePage.test.tsx
@@ -1,18 +1,20 @@
 import '@testing-library/jest-dom';
-import { screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import { renderWithProviders } from '../lib';
 import { HomePage } from '@pages/HomePage';
 
 describe('HomePage component', () => {
+  beforeAll(async () => {
+    await act(async () => {
+      renderWithProviders(<HomePage />);
+    });
+  });
   /**
    * Тест для компонента HomePage.
    * Проверяет, что компонент рендерится с заголовком "REST/GraphiQL Client".
-   *
-   * @returns {void}
+
    */
   it('renders with include the title', () => {
-    renderWithProviders(<HomePage />);
-
     const title = screen.getByRole('heading', { name: 'REST/GraphiQL Client' });
     expect(title).toBeInTheDocument();
   });
--- src/shared/tests/__tests__/Logout.test.tsx
+++ src/shared/tests/__tests__/Logout.test.tsx
@@ -1,23 +1,22 @@
 import '@testing-library/jest-dom';
-import { act, fireEvent, screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import { renderWithProviders } from '../lib';
 import { LogoutButton } from '@features/auth';
 
 describe('Logout component', () => {
+  beforeAll(async () => {
+    await act(async () => {
+      renderWithProviders(<LogoutButton />);
+    });
+  });
+
   /**
    * Тест для компонента Logout.
    * Проверяет, что кнопка "Logout" рендерится и может быть нажата.
-   *
-   * @returns {void}
    */
   it('renders with the Logout button', () => {
-    renderWithProviders(<LogoutButton />);
-
     const button = screen.getByTestId('logout-button');
-    expect(button).toBeInTheDocument();
 
-    act(() => {
-      fireEvent.click(button);
-    });
+    expect(button).toBeDisabled();
   });
 });
