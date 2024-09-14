fa4578624d82a4d56298cfa94658407dc24787e9
Jamalutdin Muslimov <mechanoid0415@gmail.com>
2024-09-14 18:00:08 +0300

    refactor: change homepage structure

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,24 +1,12 @@
 import { EditorsGroup } from '@features/editor/ui';
-import { Typography } from '@mui/material';
-import Grid from '@mui/material/Unstable_Grid2';
+import { Container, Stack } from '@mui/material';
 
 export const HomePage = () => {
   return (
-    <Grid
-      container
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      width="100%"
-      spacing={4}
-      sx={{ m: 0, position: 'relative' }}
-    >
-      <Grid>
-        <Typography variant="h4" fontWeight="bold" fontStyle="italic">
-          REST/GraphiQL Client
-        </Typography>
+    <Container>
+      <Stack gap={2} alignItems={'center'}>
         <EditorsGroup />
-      </Grid>
-    </Grid>
+      </Stack>
+    </Container>
   );
 };
