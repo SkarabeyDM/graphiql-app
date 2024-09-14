861ff6a1bf97440eeed879a2659a92df1487022d
Jamalutdin Muslimov <mechanoid0415@gmail.com>
2024-09-14 22:37:51 +0300

    feat: add authentication offer on `<HomePage />`

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,14 +1,30 @@
+'use client';
+import { useAuthState } from '@entities/user';
 import { EditorsGroup } from '@features/editor/ui';
 import { Container, Stack } from '@mui/material';
+import { Button } from '@shared/ui';
 import { Authors } from '@widgets/Authors';
 import { Greeting } from '@widgets/Greeting';
+import { useTranslations } from 'next-intl';
 
 export const HomePage = () => {
+  const t = useTranslations('Auth');
+  const [user] = useAuthState();
+
   return (
     <Container>
       <Stack gap={2} alignItems={'center'}>
         <Greeting />
-        <EditorsGroup />
+        {user ? (
+          <EditorsGroup />
+        ) : (
+          <Stack direction="row" gap={5}>
+            <Button variant="contained" href="/login">
+              {t('login')}
+            </Button>
+            <Button href="/register">{t('register')}</Button>
+          </Stack>
+        )}
         <Authors />
       </Stack>
     </Container>
